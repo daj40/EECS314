53e25285ae46c001bfbec1c2daf3855cf9be805f
Devin Schwab <digidevin@gmail.com>
2014-04-03 13:00:44 -0400

    Now will refresh the screen everytime enter is pressed. There is the issue that screen refreshing requires a different number of lines between linux and mac

--- src/main.s
+++ src/main.s
@@ -1,3 +1,4 @@
+	.text
 	.globl main
 main:
 	# stick the resturn address on the stack
@@ -15,9 +16,7 @@ main:
 	la $a0, clear_screen
 	syscall
 
-	la $a0, test_str
-	li $a1, 10
-	jal refresh_screen
+	jal read_command
 
 	# restore the resturn address and return
 	lw $ra, 0($sp)
@@ -27,26 +26,48 @@ main:
 
 read_command:
 	li $t0, 0xA
-	
+	la $t1, input_string # start of the input buffer
 
+	# now loop reading characters until a newline is detected
 read_command_loop:
 	li $v0, 12
 	syscall
 
-	la $t1, input_length
+	sb $v0, ($t1)
+	addi $t1, $t1, 1
 	
-	
-	bne $a0, $t0, read_command_loop
+	bne $v0, $t0, read_command_loop
+
+	# terminate string
+	add $t2, $zero, $zero # null terminater
+	sb $t2, ($t1)
+	addi $t1, $t1, 1
+	la $a0, input_string
+	add $a1, $t1, $zero
+
+	# stick the resturn address on the stack
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+
+	jal refresh_screen
+
+	# restore the resturn address and return
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+
+	j read_command
+	jr $ra
+	nop
 
 refresh_screen:
-	add $t0, $zero, $a0
+	add $t2, $zero, $a0
 	
 	li $v0, 4
 	la $a0, input_prompt
 	syscall
 
 	li $v0, 4
-	add $a0, $t0, $zero
+	add $a0, $t2, $zero
 	syscall
 
 	li $v0, 11
@@ -57,7 +78,8 @@ refresh_screen:
 	# to redraw the screen
 	addi $t0, $zero, 30
 	addi $t1, $zero, 50
-	div $a1, $t1
+	sub $t2, $a1, $t2
+	div $t2, $t1
 	mflo $t1
 	sub $t0, $t0, $t1
 	li $v0, 11
